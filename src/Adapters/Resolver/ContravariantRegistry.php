4d456869562bc7c6ae6f3ddbef7cd92f15d2e4b2
JuraSciix <jurasciix@gmail.com>
2026-02-23 09:39:47 +0300

    Рефакторинг: удаляет использование устаревших структур в имплементациях `RegistryInterface`

--- src/Adapters/Resolver/ContravariantRegistry.php
+++ src/Adapters/Resolver/ContravariantRegistry.php
@@ -2,31 +2,38 @@
 
 namespace JuraSciix\DataMapper\Adapters\Resolver;
 
-use JuraSciix\DataMapper\Utils\ContravariantMap;
+use InvalidArgumentException;
+use JuraSciix\DataMapper\Utils\TypeHelper;
 
 /**
  * @template TValue
  * @template-implements RegistryInterface<TValue
  */
 final class ContravariantRegistry implements RegistryInterface {
-    private readonly ContravariantMap $map;
-
-    function __construct() {
-        $this->map = new ContravariantMap();
-    }
+    /** @var array<string, TValue> */
+    private $map = [];
 
     /**
      * @inheritDoc
      */
     function insert(mixed $key, mixed $value): void {
-        $this->map->put($key, $value);
+        if (!is_string($key) || !TypeHelper::isValidType($key)) {
+            throw new InvalidArgumentException("Invalid type: {$key}");
+        }
+
+        $this->map[$key] = $value;
+        foreach (TypeHelper::getSuperTypes($key) as $superType) {
+            $this->map[$superType] = $value;
+        }
     }
 
     /**
      * @inheritDoc
      */
     function find(mixed $key): mixed {
-        if (!$this->map->contains($key)) return null;
-        return $this->map->get($key);
+        if (!is_string($key) || !TypeHelper::isValidType($key)) {
+            return null;
+        }
+        return $this->map[$key] ?? null;
     }
 }
--- src/Adapters/Resolver/CovariantRegistry.php
+++ src/Adapters/Resolver/CovariantRegistry.php
@@ -2,31 +2,64 @@
 
 namespace JuraSciix\DataMapper\Adapters\Resolver;
 
-use JuraSciix\DataMapper\Utils\CovariantMap;
+use InvalidArgumentException;
+use JuraSciix\DataMapper\Utils\TypeHelper;
 
 /**
  * @template TValue
  * @template-implements RegistryInterface<TValue
  */
 final class CovariantRegistry implements RegistryInterface {
-    private readonly CovariantMap $map;
-
-    function __construct() {
-        $this->map = new CovariantMap();
-    }
+    /** @var array<class-string, TValue> */
+    private $classes = [];
+    private $interfaces = [];
 
     /**
      * @inheritDoc
      */
     function insert(mixed $key, mixed $value): void {
-        $this->map->put($key, $value);
+        if (!is_string($key) || !TypeHelper::isValidType($key)) {
+            throw new InvalidArgumentException("Invalid type: {$key}");
+        }
+
+        if (class_exists($key)) {
+            $this->classes[$key] = $value;
+        }
+
+        if (interface_exists($key)) {
+            $this->interfaces[$key] = $value;
+        }
     }
 
     /**
      * @inheritDoc
      */
     function find(mixed $key): mixed {
-        if (!$this->map->contains($key)) return null;
-        return $this->map->get($key);
+        if (!is_string($key) || !TypeHelper::isValidType($key)) {
+            return null;
+        }
+
+        if (class_exists($key)) {
+            $current = $key;
+            do {
+                if (array_key_exists($current, $this->classes)) {
+                    return $this->classes[$current];
+                }
+                $current = get_parent_class($current);
+            } while ($current !== false);
+        }
+
+        if (interface_exists($key)) {
+            if (array_key_exists($key, $this->interfaces)) {
+                return $this->interfaces[$key];
+            }
+            foreach (class_implements($key) as $interface) {
+                if (array_key_exists($interface, $this->interfaces)) {
+                    return $this->interfaces[$key];
+                }
+            }
+        }
+
+        return null;
     }
 }
