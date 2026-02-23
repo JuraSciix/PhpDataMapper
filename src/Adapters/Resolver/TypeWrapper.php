51313255ae77843b27971243ab5bccc61b659469
JuraSciix <jurasciix@gmail.com>
2026-02-23 14:47:13 +0300

    Рефакторинг!: помечает класс `TypeWrapper` как нерасширяемый

--- src/Adapters/Resolver/TypeWrapper.php
+++ src/Adapters/Resolver/TypeWrapper.php
@@ -5,12 +5,12 @@ namespace JuraSciix\DataMapper\Adapters\Resolver;
 use JuraSciix\DataMapper\Utils\DocTypeHelper;
 use PHPStan\PhpDocParser\Ast\Type\TypeNode;
 
-class TypeWrapper {
+final class TypeWrapper {
     readonly TypeNode $sourceNode;
     readonly TypeNode $node;
     readonly string $string;
 
-    public function __construct(TypeNode $typeNode) {
+    function __construct(TypeNode $typeNode) {
         $this->sourceNode = $typeNode;
         $tn = DocTypeHelper::canonize($typeNode);
         $this->node = $tn;
